e604cc095033b5a832ddcb1784e6f6ea07f4d43e
talforte <talforte1000@gmail.com>
2025-03-15 17:13:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from view import view
+# יצרנו את מודל הסיכון אבל אנחנו לא מיישמים אותו בקוד
+# אולמה?
+# לסדר את הCONTROLLER
+# לבדוק אם המודל סיכון מחשב כמו שצריך)
 
 def main():
-    #menu = view()
-    #menu.show_menu()
-    pass
+    menu = view()
+    menu.show_menu()
+   
 
 
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     main()
